c1cfe4bbdbbfdba58ca8bc831e54c07c46c81a64
Laura Joy Erb <laurajoy@bu.edu>
2020-02-18 16:36:08 -0500

    added callback function

--- video.py
+++ video.py
@@ -4,24 +4,28 @@ import time
 
 q = queue.Queue(maxsize=5)
 
-def get_feed(q, i):
+def callback():
+    print("A thread has finished")
+
+def get_feed(q, i, callback):
     while True:
         foo = q.get()
         time.sleep(0.5)
         print(foo, end=", from thread ")
         print(i)
         q.task_done()
+        callback()
 
 def queue_module(index):
     q.put(index)
-    worker = threading.Thread(target=get_feed, args=(q,index))
+    worker = threading.Thread(target=get_feed, args=(q, index, callback))
     index += 1
     worker.setDaemon(True)
     worker.start()
 
-queue_module(1)
-queue_module(2)
-queue_module(3)
+# Main
+for val in range(20):
+    queue_module(val)
 
 q.join()
 print("Done!")

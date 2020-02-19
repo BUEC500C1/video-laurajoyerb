e812921148855256b84c9948c6d147b0d3d3eb42
Laura Joy Erb <laurajoy@bu.edu>
2020-02-19 18:03:50 -0500

    helper functions for clarity

--- video.py
+++ video.py
@@ -47,32 +47,47 @@ def format_tweet_text(text):
     else:
         return text, 1
 
+def dated_tweets(tweets):
+    dated = []
+    for tweet in tweets:
+        # checks if tweet was from the last 24 hours
+        if (datetime.datetime.now() - tweet.created_at).days < 1:
+            dated.append(tweet)
 
-# Main
-# OAuth process, using the keys and tokens
-auth = tw.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+    return dated
 
-# Creation of the actual interface, using authentication
-api = tw.API(auth)
+def get_tweets(user_name):
 
-allTweets = api.user_timeline(screen_name='@NatGeo',
-                      tweet_mode="extended", count=100)
+    # OAuth process, using the keys and tokens
+    auth = tw.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-index = 0
-for tweet in allTweets:
-    if (datetime.datetime.now() - tweet.created_at).days < 1:
-        print(tweet.full_text)
-        print("\n")
+    # Creation of the actual interface, using authentication
+    api = tw.API(auth)
+
+    allTweets = api.user_timeline(screen_name=user_name,
+                                tweet_mode="extended", count=100)
+
+    day_tweets = dated_tweets(allTweets)
+
+    index = 0
+    for tweet in day_tweets:
         wrapped_text, new_lines = format_tweet_text(tweet.full_text)
-        img_height = 20*new_lines if new_lines > 4 else 20*new_lines + 5
+
+        img_height = 20 * new_lines if new_lines > 4 else 20 * new_lines + 5
         img = Image.new('RGB', (200, img_height), (255, 255, 255))
+
         d = ImageDraw.Draw(img)
         d.text((10, 10), wrapped_text.encode(
             'cp1252', 'ignore'), fill=(0, 0, 0))
-        image_name = "image" + str(index) + ".png"
+
+        image_name = "tweet" + str(index) + ".png"
         img.save(image_name)
         index += 1
+
+
+# Main
+get_tweets('@NatGeo')
 
 q.join()
 print("Done!")

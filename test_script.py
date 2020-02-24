2d7279fb133345926a8c8937540d000e31bc7938
Laura Joy Erb <laurajoy@bu.edu>
2020-02-24 17:23:36 -0500

    imported all functions

--- test_script.py
+++ test_script.py
@@ -1,4 +1,7 @@
 from video import send_completed_video
+from video import format_tweet_text, dated_tweets, add_media, no_tweets_error
+from video import get_tweet_images, get_tweets
+from video import clean_all, clean_old
 
 def test1():
     assert 1 == 1

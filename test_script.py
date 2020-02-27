7fa95e15329b599d756d183afdf9f5c1d34b1f61
Laura Joy Erb <laurajoy@bu.edu>
2020-02-27 00:01:22 -0500

    tests no_tweet_error

--- test_script.py
+++ test_script.py
@@ -3,5 +3,36 @@ from video import format_tweet_text, dated_tweets, add_media, no_tweets_error
 from video import get_tweet_images, get_tweets
 from video import clean_all, clean_old
 
-def test1():
-    assert 1 == 1
+import os
+
+import globals
+
+# def test1():
+#     # setup
+#     globals.init()
+
+#     call = {
+#         "user_name": "NatGeo",
+#         "id": 0,
+#         "status": "queued"
+#     }
+#     globals.q.put(call)
+#     globals.processes["0"] = call
+
+#     # get_tweets()
+
+#     # assert globals.processes["0"]["status"] == "completed"
+#     assert 1 == 1
+
+def test_error_image():
+    no_tweets_error("LauraJoy", "314")
+
+    file_exists = os.path.isfile(
+        "/Users/laurajoyerb/Documents/MyCode/EC500/video-laurajoyerb/314LauraJoy_tweet0.png")
+
+    if file_exists:
+        os.remove("/Users/laurajoyerb/Documents/MyCode/EC500/video-laurajoyerb/314LauraJoy_tweet0.png")
+
+    assert file_exists == True
+
+    
